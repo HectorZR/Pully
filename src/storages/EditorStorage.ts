8cce5af31f440ac81382aa1c22a0589fcaa3fbfc
Hector Zurga <hjzurga@gmail.com>
2023-12-09 19:58:19 -0400

    refact(EditorStorage): added to get access to global or secret storage

---
+++ src/storages/EditorStorage.ts
@@ -0,0 +1,23 @@
+import { contextHandler } from '../handlers/ContextHandler';
+
+type StorageType = 'global' | 'secret';
+
+export class EditorStorage {
+	static get(key: string, storage: StorageType = 'global') {
+		const context = contextHandler.getContext();
+		if (storage === 'secret') {
+			return context.secrets.get(key);
+		}
+		return context.globalState.get(key);
+	}
+
+	static set(key: string, value: unknown, storage: StorageType = 'global') {
+		const context = contextHandler.getContext();
+		if (storage === 'secret') {
+			context.secrets.store(key, value as string);
+			return;
+		}
+
+		context.globalState.update(key, value);
+	}
+}
